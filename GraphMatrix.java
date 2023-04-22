8fc8a22a28ae68a4dd27227e6cebf51675b454a5
YshikageOne <124365039+YshikageOne@users.noreply.github.com>
2023-04-23 03:23:01 +0800

    Update GraphMatrix.java

--- GraphMatrix.java
+++ GraphMatrix.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/*
+*2. Write a java program that accepts an adjacency matrix of a graph. The program should list the edges of this graph and give
+the number of times each edge appears.
+*/
+
 public class GraphMatrix {
 
 	public static void main(String[] args) {
